74c41e4172db0bf100fda41550d033d893990b99
EDISSONG <eagarcia@soaint.com>
2020-04-22 10:45:05 -0500

    trace

--- src/app/Services/interceptor/interceptor.service.ts
+++ src/app/Services/interceptor/interceptor.service.ts
@@ -14,6 +14,7 @@ export class InterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token: string = localStorage.getItem('token');
+    console.log('token ', token);
     let request = req;
 
     if (!req.headers.get('authorization')) {
